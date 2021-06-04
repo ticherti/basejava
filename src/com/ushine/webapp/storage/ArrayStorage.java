5f60626f2bb590b7a1f182ac54f5a52c8bd15a1f
Ticherti <vera.karr@gmail.com>
2021-06-04 21:59:41 +0300

    Rename firstEmptyCell into size Rename position into index in get() Add console messages Move update() upper in the list

--- src/com/ushine/webapp/storage/ArrayStorage.java
+++ src/com/ushine/webapp/storage/ArrayStorage.java
@@ -8,70 +8,81 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    private int firstEmptyCell = 0;
+    Resume[] storage = new Resume[10_000];
+    private int size = 0;
 
     public void clear() {
-        Arrays.fill(storage, 0, firstEmptyCell, null);
-        firstEmptyCell = 0;
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     public void save(Resume r) {
         if (r == null) return;
-        if (firstEmptyCell == storage.length) return;
-        if (findResume(r.getUuid()) == -1) {
-            storage[firstEmptyCell] = r;
-            firstEmptyCell++;
+        String uuid = r.getUuid();
+        if (size == storage.length) {
+            System.out.println("The storage is full. The resume hasn't been added. Uuid: " + uuid);
+            return;
+        }
+        if (findResume(uuid) == -1) {
+            storage[size] = r;
+            size++;
+            System.out.println("Successfully added the resume. Uuid: " + uuid);
+        } else {
+            System.out.println("There is already such resume in the storage. Uuid: " + uuid);
         }
     }
 
     public Resume get(String uuid) {
         if (uuid == null) return null;
-        int position = findResume(uuid);
-        if (position > -1) {
-            return storage[position];
+        int index = findResume(uuid);
+        if (index > -1) {
+            System.out.println("That's the resume. Uuid: " + uuid);
+            return storage[index];
         }
+        System.out.println("No such resume in the storage. Uuid: " + uuid);
         return null;
+    }
+
+    public void update(Resume resume) {
+        if (resume == null) {
+            System.out.println("Not found for the update");
+            return;
+        }
+        if (findResume(resume.getUuid()) > -1) {
+            System.out.println("The resume has been updated. Uuid: " + resume.getUuid());
+        }
     }
 
     public void delete(String uuid) {
         if (uuid == null) return;
-        int position = findResume(uuid);
-        if (position > -1) {
-            storage[position] = null;
-            int lastEntry = firstEmptyCell - 1;
-            if (lastEntry - position >= 0)
-                System.arraycopy(storage, position + 1, storage, position, lastEntry - position);
+        int index = findResume(uuid);
+        if (index > -1) {
+            storage[index] = null;
+            int lastEntry = size - 1;
+            if (lastEntry - index >= 0)
+                System.arraycopy(storage, index + 1, storage, index, lastEntry - index);
             storage[lastEntry] = null;
-            firstEmptyCell--;
+            size--;
+            System.out.println("The resume has been deleted. Uuid: " + uuid);
         }
+        System.out.println("There's no such resume in the storage. Uuid: " + uuid);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] allResumes = new Resume[firstEmptyCell];
-        System.arraycopy(storage, 0, allResumes, 0, firstEmptyCell);
+        Resume[] allResumes = new Resume[size];
+        System.arraycopy(storage, 0, allResumes, 0, size);
         return allResumes;
     }
 
     public int size() {
-        return firstEmptyCell;
-    }
-
-    public void update(Resume resume) {
-        if (resume == null) {
-            System.out.println("Not found for the update.");
-            return;
-        }
-        if (findResume(resume.getUuid()) > -1) {
-            System.out.println("Resume's been updated.");
-        }
+        return size;
     }
 
     private int findResume(String uuid) {
-        for (int i = 0; i < firstEmptyCell; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].getUuid() == uuid) {
                 return i;
             }
