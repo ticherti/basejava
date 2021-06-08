16f511736d6365b398b416bd5d4aa09e2fe0aabf
Ticherti <vera.karr@gmail.com>
2021-06-08 08:18:16 +0300

    Bring files into line with the commits in Lesson03 implements interface
    - add Storage interface
    - ArrayStorage implements Storage

---
+++ src/com/ushine/webapp/storage/Storage.java
@@ -0,0 +1,19 @@
+package com.ushine.webapp.storage;
+
+import com.ushine.webapp.model.Resume;
+
+public interface Storage {
+    void clear();
+
+    void update(Resume r);
+
+    void save(Resume r);
+
+    Resume get(String uuid);
+
+    void delete(String uuid);
+
+    Resume[] getAll();
+
+    int size();
+}
