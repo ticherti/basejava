e76fa3aafb4fb967c15c41751071990ba6da06ef
Ticherti <vera.karr@gmail.com>
2021-06-11 16:31:28 +0300

    Invokation of toString in Mainreflexion

--- src/com/ushine/webapp/MainReflection.java
+++ src/com/ushine/webapp/MainReflection.java
@@ -3,17 +3,26 @@ package com.ushine.webapp;
 import com.ushine.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
-        // TODO : invoke r.toString via reflection
+        // toString through reflexion. Perversity.
+        Method[] mas = r.getClass().getDeclaredMethods();
+        for (Method m : mas) {
+            if (m.getName().equals("toString")) {
+                System.out.println(m.invoke(r));
+                break;
+            }
+        }
         System.out.println(r);
     }
 }
