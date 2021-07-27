74f53e8f85b2f7088084aded0b1cbded5805d1bf
Ticherti <vera.karr@gmail.com>
2021-07-27 11:41:19 +0300

    HW 10 - H11 DeadLock refactor

--- src/com/ushine/webapp/MainDeadLock.java
+++ src/com/ushine/webapp/MainDeadLock.java
@@ -1,38 +1,27 @@
 package com.ushine.webapp;
 
 public class MainDeadLock {
-    static final Object lock1 = new Object();
-    static final Object lock2 = new Object();
+    static final Object lock1 = "lock 1";
+    static final Object lock2 = "lock 2";
 
     public static void main(String[] args) {
-
-        new Thread(() -> {
-            synchronized (lock1) {
-                System.out.println(Thread.currentThread() + " locks A");
-                sleep();
-                System.out.println("Waiting for B to unlock");
-                synchronized (lock2) {
-                    System.out.println(Thread.currentThread() + " locks B");
-                }
-            }
-        }).start();
-        new Thread(() -> {
-            synchronized (lock2) {
-                System.out.println(Thread.currentThread() + " locks B");
-                sleep();
-                System.out.println("Waiting for A to unlock");
-                synchronized (lock1) {
-                    System.out.println(Thread.currentThread() + " locks A");
-                }
-            }
-        }).start();
+        new Thread(() -> lockThem(lock1, lock2)).start();
+        new Thread(() -> lockThem(lock2, lock1)).start();
     }
 
-    private static void sleep() {
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    private static void lockThem(Object firstLock, Object secondLock) {
+        synchronized (firstLock) {
+            String name = Thread.currentThread().getName();
+            System.out.println(name + " locked " + firstLock);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(name + "'s waiting for " + secondLock + " to be unlocked");
+            synchronized (secondLock) {
+                System.out.println(name + " " + secondLock);
+            }
         }
     }
 }
